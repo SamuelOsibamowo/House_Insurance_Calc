8c2b4c2bdc37b82fbec585d3edf38cc2a7a4a60b
Samuel Osibamowo <samuelosibamowo@gmail.com>
2021-05-13 18:00:12 -0500

    Initial commit

---
+++ app/src/main/java/com/example/house_insurance_calc/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.house_insurance_calc
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.house_insurance_calc.databinding.ActivityMainBinding
+import java.text.NumberFormat
+import kotlin.math.pow
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.calculatePayment.setOnClickListener { calculateInterest() }
+
+    }
+
+    private fun calculateInterest() {
+        val stringInTextField = binding.housePrice.text.toString()
+        val price = stringInTextField.toDoubleOrNull() ?: return
+
+        var interest = when(binding.thirtyYearInterestRates.checkedRadioButtonId){
+            R.id.BOA_interest_30 -> .0275
+            else -> .03
+        }
+        interest /= 12
+
+        var payment = price*(((1+interest).pow(360))*interest)
+        var payment2 = ((1+interest).pow(360))-1
+        var final = payment/payment2
+
+
+        NumberFormat.getCurrencyInstance()
+        val formattedPayment = NumberFormat.getCurrencyInstance().format(final)
+
+        binding.thePayment.text = formattedPayment.toString()
+
+
+
+
+    }
+
+
+}
